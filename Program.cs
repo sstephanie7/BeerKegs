d3d025c003f32d8221e8dbd112ddc911d7352716
Stephanie Tahova <106015292+sstephanie7@users.noreply.github.com>
2022-06-07 16:06:21 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace BeerKegs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int nLines = int.Parse(Console.ReadLine());
+
+            double biggestKeg = double.MinValue;
+            string biggestKegName = "";
+            for (int i = 0; i < nLines; i++)
+            {
+
+                string modelOfKeg = Console.ReadLine();
+                float radius = float.Parse(Console.ReadLine());
+                int height = int.Parse(Console.ReadLine());
+                double volumeOfCurrentKeg = Math.PI * Math.Pow(radius, 2) * height;
+
+                if (volumeOfCurrentKeg > biggestKeg)
+                {
+                    biggestKeg = volumeOfCurrentKeg;
+                    biggestKegName = modelOfKeg; 
+                }
+            }
+            Console.WriteLine(biggestKegName);
+
+        }
+    }
+}
